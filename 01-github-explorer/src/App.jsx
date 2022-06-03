dd2fdeb2ac12cef849daf4a70a6bcea91473764d
Vially Israel <viallyisrael@hotmail.com>
2022-06-03 17:35:40 -0300

    finishing cap 1 - unit 3

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,12 +1,7 @@
-import { Counter } from './componentes/Counter';
 import { RepositoryList } from './componentes/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
-  return  (
-    <>
-      <RepositoryList/>
-      <Counter />
-  </>
+  return  (      <RepositoryList/>
   )
 }
--- 01-github-explorer/src/componentes/RepositoryList.jsx
+++ 01-github-explorer/src/componentes/RepositoryList.jsx
@@ -1,20 +1,25 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useState, useEffect } from 'react';
 
-const repository ={
-  name:'unform',
-  description: "Forms in React",
-  link: "https://github.com/unform/unform"
-}
+//https://api.github.com/users/viisrael/repos
+
 export function RepositoryList(){
+  const[repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/viisrael/repos').then(response => response.json())
+                                                        .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
